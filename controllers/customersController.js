43d023c801c7e74c4023f50c5ed511b703c3c604
NadaSGouda <153154088+NadaSGouda@users.noreply.github.com>
2023-12-08 15:54:30 +0200

    Update customersController.js

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -8,20 +8,30 @@ class customersController{
         }
     }
 
-    static async addNewCustomer(req, res){
+     static async addNewCustomer(req, res){
         var arname = req.body.customer_arname;
         var enname = req.body.customer_enname;
         var activity = req.body.activity_id;
         var address = req.body.customer_address;
         var phone = req.body.customer_phone;
         var notes = req.body.notes;
-        var result = await customerModel.addNewCustomer(arname, enname, activity, address, phone, notes);
-        if(result == true){
-            res.send("added successfully")
-        }else{
-            res.send("added failed")
-        }
+        if (arname, enname, activity, address, phone, notes) {
+            try {
+                var result = await customerModel.addNewCustomer(arname, enname, activity, address, phone, notes);        
+              if (result) {
+                res.status(200).json({ status: 'success', message: 'Added successfully' });
+              } else {
+                res.status(400).json({ status: 'error', message: 'Add failed' });
+              }
+            } catch (error) {
+              console.error(error);
+              res.status(500).json({ status: 'error', message: 'Internal server error' });
+            }
+          } else {
+            res.status(400).json({ status: 'error', message: 'Invalid request' });
+          }
     }
+    
 
    static async deleteCustomer(req, res) {
         const id = req.params.id;
@@ -54,7 +64,7 @@ class customersController{
         }
     }
 
-    static async editCustomer(req, res){
+   static async editCustomer(req, res) {
         const id = req.params.id;
         const newarname = req.body.customer_arname;
         const newenname = req.body.customer_enname;
@@ -62,13 +72,23 @@ class customersController{
         const newaddress = req.body.customer_address;
         const newphone = req.body.customer_phone;
         const newnotes = req.body.notes;
-
-        var result = await customerModel.editCustomer(id, newarname, newenname, newactivityid, newaddress, newphone, newnotes);
-        if(result){
-            res.send("customer edited successfully")
-        }else{
-            res.send("failed")
-        }
+      
+        if (id) {
+          try {
+            const result = await customerModel.updateCustomer(id, newarname, newenname, newactivityid, newaddress, newphone, newnotes);
+      
+            if (result) {
+              res.status(200).json({ status: 'success', message: 'Updated successfully' });
+            } else {
+              res.status(400).json({ status: 'error', message: 'Update failed' });
+            }
+          } catch (error) {
+            console.error(error);
+            res.status(500).json({ status: 'error', message: 'Internal server error' });
+          }
+        } else {
+          res.status(400).json({ status: 'error', message: 'Invalid request: Missing ID or updated data' });
+        }
     }
 }
 
