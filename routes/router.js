8f8a1ea5f70a8f82c1da0f235079c4b74c3fc49e
NadaSGouda <153154088+NadaSGouda@users.noreply.github.com>
2023-12-08 10:41:21 +0200

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -11,27 +11,27 @@ router.get("/", (req, res, next)=>{
 
 router.get("/customers", customerController.getAllCustomers)
 router.post("/addCustomer", customerController.addNewCustomer)
-router.post("/deleteCustomer", customerController.deleteCustomer)
+router.post("/deleteCustomer/:id", customerController.deleteCustomer)
 router.get("/getSpecificCustomer/:id", customerController.getSpecificCustomer)
-router.post("/editCustomer", customerController.editCustomer)
+router.post("/editCustomer/:id", customerController.editCustomer)
 
 router.get("/activities", activitiesController.getAllactivities)
 router.post("/addActivity", activitiesController.addNewactivity)
-router.post("/deleteActivity", activitiesController.deleteactivity)
-router.get("/getSpecificActivity", activitiesController.getSpecificactivity)
-router.post("/editActivity", activitiesController.editactivity)
+router.post("/deleteActivity/:id", activitiesController.deleteactivity)
+router.get("/getSpecificActivity/:id", activitiesController.getSpecificactivity)
+router.post("/editActivity/:id", activitiesController.editactivity)
 
 router.get("/employees", employeesController.getAllemployees)
 router.post("/addEmployee", employeesController.addNewEmployee)
-router.post("/deleteEmployee", employeesController.deleteemployee)
-router.get("/getSpecificEmployee", employeesController.getSpecificemployee)
-router.post("/editEmployee", employeesController.editEmployee)
+router.post("/deleteEmployee/:id", employeesController.deleteemployee)
+router.get("/getSpecificEmployee/:id", employeesController.getSpecificemployee)
+router.post("/editEmployee/:id", employeesController.editEmployee)
 
 router.get("/quotations", quotationsController.getAllquotations)
 router.post("/addQuotation", quotationsController.addNewquotation)
-router.post("/deleteQuotation", quotationsController.deletequotation)
-router.get("/getSpecificQuotation", quotationsController.getSpecificquotation)
-router.post("/editQuotation", quotationsController.editQuotation)
+router.post("/deleteQuotation/:id", quotationsController.deletequotation)
+router.get("/getSpecificQuotation/:id", quotationsController.getSpecificquotation)
+router.post("/editQuotation/:id", quotationsController.editQuotation)
 router.get("/searchQuotation", quotationsController.searchQuotation)
 
 module.exports = router;
