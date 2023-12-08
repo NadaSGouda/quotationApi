ca3ff5ca45ac95375e15070a4664d28b144fea6b
NadaSGouda <153154088+NadaSGouda@users.noreply.github.com>
2023-12-08 16:07:48 +0200

    Update employeesController.js

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -8,28 +8,44 @@ class employeesController{
         }
     }
 
-    static async addNewEmployee(req, res){
+   static async addNewEmployee(req, res){
         var employee_name = req.body.employee_name;
         var result = await employeeModel.addNewemployee(employee_name);
-        if(result == true){
-            res.send("added successfully")
-        }else{
-            res.send("added failed")
-        }
-    }
-
-    static async deleteemployee(req, res){
-        const id = req.params.id;
-        if(id){
-            var result = await employeeModel.deleteemployee(id);
-            if(result){
-                res.send("deleted successfully")
-            }else{
-                res.send("failed")
+        if (employee_name) {
+            try {
+              var result = await employeeModel.addNewemployee(employee_name);
+              if (result) {
+                res.status(200).json({ status: 'success', message: 'Added successfully' });
+              } else {
+                res.status(400).json({ status: 'error', message: 'Add failed' });
+              }
+            } catch (error) {
+              console.error(error);
+              res.status(500).json({ status: 'error', message: 'Internal server error' });
             }
+        } else {
+            res.status(400).json({ status: 'error', message: 'Invalid request' });
         }
     }
 
+     static async deleteemployee(req, res){
+        const id = req.params.id;
+        if (id) {
+            try {
+                var result = await employeeModel.deleteemployee(id);
+              if (result) {
+                res.status(200).json({ status: 'success', message: 'Deleted successfully' });
+              } else {
+                res.status(400).json({ status: 'error', message: 'Delete failed' });
+              }
+            } catch (error) {
+              console.error(error);
+              res.status(500).json({ status: 'error', message: 'Internal server error' });
+            }
+        } else {
+            res.status(400).json({ status: 'error', message: 'Invalid request' });
+        }
+    }
     static async getSpecificemployee(req, res){
         const id = req.params.id;
         if(id){
@@ -44,11 +60,20 @@ class employeesController{
         const id = req.params.id;
         const newEmployeename = req.body.employee_name;
 
-        var result = await employeeModel.editEmployee( newEmployeename, id);
-        if(result){
-            res.send("activity edited successfully")
-        }else{
-            res.send("failed")
+        if (id, newEmployeename) {
+            try {
+                var result = await employeeModel.editEmployee( newEmployeename, id);
+              if (result) {
+                res.status(200).json({ status: 'success', message: 'Updated successfully' });
+              } else {
+                res.status(400).json({ status: 'error', message: 'Update failed' });
+              }
+            } catch (error) {
+              console.error(error);
+              res.status(500).json({ status: 'error', message: 'Internal server error' });
+            }
+        } else {
+            res.status(400).json({ status: 'error', message: 'Invalid request' });
         }
     }
 }
