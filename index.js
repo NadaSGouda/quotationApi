6336f4fce87a64cd7cf7c6b07b0701992719d612
NadaSGouda <153154088+NadaSGouda@users.noreply.github.com>
2023-12-08 08:38:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const mydb = require("./config/db");
-const dotenv = require('dotenv').config()
 const route = require("./routes/router");
+const cors = require("cors");
 const bodyparser = require("body-parser");
-app.use(express.json());
-app.use(bodyparser.urlencoded({extended:true}));
-app.use(route)
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(route);
 
 
-app.listen(process.env.PORT, ()=>{
+
+
+app.listen( 3306 , () => {
     console.log("server is running")
 })
