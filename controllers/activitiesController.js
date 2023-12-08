f33d578a4db076216cbdd2ea904c54886b78946d
NadaSGouda <153154088+NadaSGouda@users.noreply.github.com>
2023-12-08 16:01:45 +0200

    Update activitiesController.js

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -8,14 +8,23 @@ class activitiesController{
         }
     }
 
-    static async addNewactivity(req, res){
+        static async addNewactivity(req, res){
         var activity_name = req.body.activity_name;
         var notes = req.body.notes;
-        var result = await activityModel.addNewactivity(activity_name, notes);
-        if(result == true){
-            res.send("added successfully")
-        }else{
-            res.send("added failed")
+        if (activity_name, notes) {
+            try {
+              var result = await activityModel.addNewactivity(activity_name, notes);        
+              if (result) {
+                res.status(200).json({ status: 'success', message: 'Added successfully' });
+              } else {
+                res.status(400).json({ status: 'error', message: 'Add failed' });
+              }
+            } catch (error) {
+              console.error(error);
+              res.status(500).json({ status: 'error', message: 'Internal server error' });
+            }
+          } else {
+            res.status(400).json({ status: 'error', message: 'Invalid request' });
         }
     }
 
@@ -29,6 +38,22 @@ class activitiesController{
                 res.send("failed")
             }
         }
+        if (id) {
+            try {
+              var result = await activityModel.deleteactivity(id);
+              if (result) {
+                res.status(200).json({ status: 'success', message: 'Deleted successfully' });
+              } else {
+                res.status(400).json({ status: 'error', message: 'Delete failed' });
+              }
+            } catch (error) {
+              console.error(error);
+              res.status(500).json({ status: 'error', message: 'Internal server error' });
+            }
+        } else {
+            res.status(400).json({ status: 'error', message: 'Invalid request' });
+        }
+        
     }
 
     static async getSpecificactivity(req, res){
@@ -41,17 +66,26 @@ class activitiesController{
         }
     }
 
-    static async editactivity(req, res){
+   static async editactivity(req, res){
         const id = req.params.id;
         const newactivityname = req.body.activity_name;
         const newnotes = req.body.notes;
-
-        var result = await activityModel.editactivity( newactivityname, newnotes, id);
-        if(result){
-            res.send("activity edited successfully")
-        }else{
-            res.send("failed")
+        if (id, newactivityname, newnotes) {
+            try {
+              var result = await activityModel.editactivity( newactivityname, newnotes, id);
+              if (result) {
+                res.status(200).json({ status: 'success', message: 'Updated successfully' });
+              } else {
+                res.status(400).json({ status: 'error', message: 'Update failed' });
+              }
+            } catch (error) {
+              console.error(error);
+              res.status(500).json({ status: 'error', message: 'Internal server error' });
+            }
+        } else {
+            res.status(400).json({ status: 'error', message: 'Invalid request' });
         }
+        
     }
 }
 
