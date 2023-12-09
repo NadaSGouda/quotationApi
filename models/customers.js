5706186a32cfb5beeb7ae4eb909bf0981b1b509b
NadaSGouda <153154088+NadaSGouda@users.noreply.github.com>
2023-12-09 09:14:42 +0200

    Update customers.js

--- models/customers.js
+++ models/customers.js
@@ -11,7 +11,6 @@ class customerModel{
                     console.error("error", error);
                     reject(error)
                 }
-                db.close();
             })
         })
     }
