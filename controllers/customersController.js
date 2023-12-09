065a6a3e88cd245610ba14a4721624f0565c63c1
NadaSGouda <153154088+NadaSGouda@users.noreply.github.com>
2023-12-09 17:34:37 +0200

    Update customersController.js

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -75,7 +75,7 @@ class customersController{
       
         if (id) {
           try {
-            const result = await customerModel.editCustomer(id, newarname, newenname, newactivityid, newaddress, newphone, newnotes);
+            const result = await customerModel.editCustomer(newarname, newenname, newactivityid, newaddress, newphone, newnotes, id);
       
             if (result) {
               res.status(200).json({ status: 'success', message: 'Updated successfully' });
